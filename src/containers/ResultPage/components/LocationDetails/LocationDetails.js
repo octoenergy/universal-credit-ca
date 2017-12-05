ab90d5620a8f3bde3fff869b19e8c1f86aeefa07
MrFirthy <ashley@mrfirthy.me>
2017-12-05 18:03:35 +0000

    Sort info box permutations and fix typo

--- src/containers/ResultPage/components/LocationDetails/LocationDetails.js
+++ src/containers/ResultPage/components/LocationDetails/LocationDetails.js
@@ -35,16 +35,17 @@ class LocationDetails extends Component {
 
         {isLive ? <div>
           <h2 className="status">The Universal Credit Full Service is <span className="green">&bull;LIVE</span> in {lad11nm}.</h2>
-          <p>Any claimant can make a claim, and claims can be made and line.</p>
+          <p>Any claimant can make a claim, and claims can be made and managed online.</p>
           <p>Visit <a href="https://www.universal-credit.service.gov.uk/">the Universal Credit website</a> to make or manage a claim.</p>
         </div> : <div>
           <h2 className="status">The Universal Credit Full Service is due to go live <span className="green">{goLiveDate}</span> in {lad11nm}.</h2>
           <p>Once it does, any claimant can make a claim, and claims can be made and managed online.</p>
         </div>}
 
-        {isComplex && <div className="info">
+        {!isLive && <div className="info">
           <h4>Currently</h4>
-          <p>Single claimants, couples and families can make a claim online. Claimants must earn less than £338 per month, after tax.</p>
+          {isComplex && <p>Single claimants, couples and families can make a claim online. Claimants must earn less than £338 per month, after tax.</p>}
+          {!isComplex && <p>Only simple claims – from those who are single, and without children or a health condition – can be made. Claimants must earn less than £338 per month, after tax.</p>}
           <p>Visit <a href="https://www.universal-credit.service.gov.uk/">the Universal Credit website</a> to make a claim.</p>
         </div> }
       </div>
