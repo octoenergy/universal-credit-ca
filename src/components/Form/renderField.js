148b0897bc13fe66fc715072388cd6709a5e4861
MrFirthy <ashley@mrfirthy.me>
2017-12-05 17:21:51 +0000

    Add form field error styles and redo layout with flex

--- src/components/Form/renderField.js
+++ src/components/Form/renderField.js
@@ -3,6 +3,6 @@ import './FormFieldStyles.scss';
 export const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
     <div className="FormField">
       <input className="FormField__Input" {...input} placeholder={label} type={type}/>
-      {touched && ((error && <span className="FormField__Error">{error}</span>) || (warning && <span>{warning}</span>))}
+      {touched && ((error && <span className="FormField__Error error">{error}</span>) || (warning && <span>{warning}</span>))}
     </div>
 );
